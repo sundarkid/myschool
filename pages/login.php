4541d59c6ac2f5750b872f5d456a328b0c8ef85e
sundarkid <sundareswaran25@gmail.com>
2015-09-24 13:49:40 +0530

    Initial

---
+++ pages/login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <?php
+    if (!isset($_POST['institution']) || $_POST['institution'] == "")
+        echo '<script type="text/javascript">location.href = "login.html";</script>';
+    else {
+        session_start();
+        $_SESSION['userId'] = $_POST['institution'];
+        $_SESSION['userName'] = $_POST['institution'];
+        $_SESSION['sessionID'] = md5($_POST['password']);
+        echo "";
+        echo '<script type="text/javascript">location.href = "index.php";</script>';
+    }
+    ?>
+
+    <title>My School</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="../dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="../bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div id="wrapper">
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <?php
+            echo '<a class="navbar-brand" href="index.php">Welcome to MySchool, ' . $_SESSION['userName'] . '. Please wait while we are logging you in.</a>';
+            ?>
+        </div>
+        <!-- /.navbar-header -->
+</div>
+
+</body>
+</html>
