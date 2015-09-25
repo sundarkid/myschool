b39a1ce5676007fc4594de69b1032a27f1e52a6f
sundarkid <sundareswaran25@gmail.com>
2015-09-25 14:17:43 +0530

    Login.php completed

--- pages/login.php
+++ pages/login.php
@@ -1,72 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <?php
-    if (!isset($_POST['institution']) || $_POST['institution'] == "")
-        echo '<script type="text/javascript">location.href = "login.html";</script>';
-    else {
-        session_start();
-        $_SESSION['userId'] = $_POST['institution'];
-        $_SESSION['userName'] = $_POST['institution'];
-        $_SESSION['sessionID'] = md5($_POST['password']);
-        echo "";
-        echo '<script type="text/javascript">location.href = "index.php";</script>';
+if (!isset($_POST['institution']) || $_POST['institution'] === "" || $_POST['password'] === "" || !isset($_POST['password']))
+    echo json_encode(array('result' => "failure"));
+else {
+
+    require "databaseAndFunctions.php";
+
+    $email = stripcslashes($_POST['institution']);
+    $password = stripcslashes($_POST['password']);
+
+    $email = $DB->real_escape_string($email);
+    $password = $DB->real_escape_string($password);
+
+    $password = md5($password . $salt);
+
+    $sql = "Select `uname`, `user_id`, `email` from `user_details` where `email` = '$email' AND `password` = '$password'";
+
+    $result = $DB->query($sql);
+
+    if ($result) {
+        $row = mysqli_fetch_array($result);
+        if ($row['email'] === $email) {
+            session_start();
+            $_SESSION['userId'] = $row['user_id'];
+            $_SESSION['userName'] = $row['uname'];
+            $_SESSION['sessionID'] = md5($row['uname']);
+            echo json_encode(array('result' => "success", 'name' => $row['uname']));
+        }
     }
-    ?>
 
-    <title>My School</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Timeline CSS -->
-    <link href="../dist/css/timeline.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Morris Charts CSS -->
-    <link href="../bower_components/morrisjs/morris.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-<div id="wrapper">
-    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <?php
-            echo '<a class="navbar-brand" href="index.php">Welcome to MySchool, ' . $_SESSION['userName'] . '. Please wait while we are logging you in.</a>';
-            ?>
-        </div>
-        <!-- /.navbar-header -->
-</div>
-
-</body>
-</html>
+}
+?>
